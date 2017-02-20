f59c4eef3ea8c069bde3dc230214e32b40b17f57
Melmarin <melmarin@hotmail.es>
2017-02-20 15:41:23 -0600

    creación docente y especialidad en domain

--- SistemaCAMEL/Domain/Estudiante.cs
+++ SistemaCAMEL/Domain/Estudiante.cs
@@ -12,7 +12,90 @@ namespace Domain
         private String cedula;
         private String nombre;
         private String apellidos;
-        private Encargado encargado;
+        private EncargadoEstudiante encargado;
+
+        public Estudiante()
+        {
+            this.Carne = "";
+            this.cedula = "";
+            this.nombre = "";
+            this.apellidos = "";
+            this.encargado = new EncargadoEstudiante(); 
+        }//constructor
+
+        public Estudiante(String carne, String cedula, String nombre, String apellidos, EncargadoEstudiante encargado)
+        {
+            this.Carne = carne;
+            this.Cedula = cedula;
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+            this.Encargado = encargado;
+        }//consturctor
+
+        public string Carne
+        {
+            get
+            {
+                return carne;
+            }
+
+            set
+            {
+                carne = value;
+            }
+        }
+
+        public string Cedula
+        {
+            get
+            {
+                return cedula;
+            }
+
+            set
+            {
+                cedula = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellidos
+        {
+            get
+            {
+                return apellidos;
+            }
+
+            set
+            {
+                apellidos = value;
+            }
+        }
+
+        public EncargadoEstudiante Encargado
+        {
+            get
+            {
+                return encargado;
+            }
+
+            set
+            {
+                encargado = value;
+            }
+        }
 
 
     }//class

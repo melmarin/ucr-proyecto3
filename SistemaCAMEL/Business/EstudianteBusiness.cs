99a220863b671388c55bdd6aa2bd5490baa84c87
Steven Camacho <stevencorreo@yahoo.com>
2017-02-20 15:05:48 -0600

    creación clase estudiante en capa business y data

---
+++ SistemaCAMEL/Business/EstudianteBusiness.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Data;
+
+namespace Business
+{
+    public class EstudianteBusiness
+    {
+
+        private EstudianteData personaData;
+
+
+        public EstudianteBusiness()
+        {
+            this.personaData = new EstudianteData();
+        }//constructor
+
+        public String insertarPersona(Estudiante estudiantes)
+        {
+            return personaData.insertarEstudiante(estudiante);
+        }
+    }//class
+}//namespace

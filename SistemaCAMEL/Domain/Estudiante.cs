94c1ac062a7b18669183f1084b3d78b7ed62906a
Melmarin <melmarin@hotmail.es>
2017-02-20 15:07:01 -0600

    Agregados estudiante y encargado al domain

---
+++ SistemaCAMEL/Domain/Estudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Estudiante
+    {
+        private String carne;
+        private String cedula;
+        private String nombre;
+        private String apellidos;
+        private Encargado encargado;
+
+
+    }//class
+}//namespace

1c7abe38402923953751e964bdb5fe1a5184c17e
Steven Camacho <stevencorreo@yahoo.com>
2017-02-23 02:58:20 -0600

    Creación insertarDocenteForm( por el momento solo recupera datos junto con las especialidades del docente seleccionado)

---
+++ SistemaCAMEL/SistemaEscolar/InsertarDocenteForm.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Domain;
+using Business;
+using System.Web.Configuration;
+
+namespace SistemaEscolar
+{
+    public partial class InsertarDocenteForm : System.Web.UI.Page
+    {
+        private String conectionString;
+        private DocenteBusiness docenteBusiness;
+        private LinkedList<Docente> listaDocentes;
+        private LinkedList<Docente> listaDocentes2;
+        public InsertarDocenteForm()
+        {
+            conectionString = WebConfigurationManager.ConnectionStrings["2017_sistema_camel"].ConnectionString;
+            docenteBusiness = new DocenteBusiness(conectionString);
+            listaDocentes = new LinkedList<Docente>();
+            listaDocentes = docenteBusiness.obtenerDocentes();
+            
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //Llenado del dropDown
+                ddlDocentes.DataSource = listaDocentes;
+                ddlDocentes.DataValueField = "cedula";
+                ddlDocentes.DataTextField = "cedula";
+                ddlDocentes.DataBind();
+                ddlDocentes.SelectedIndex = ddlDocentes.Items.Count - 1;
+            }
+        }//page_load
+
+        protected void btn_consultar_Click(object sender, EventArgs e)
+        {
+
+            Docente docente = new Docente();
+            string valorCedula = ddlDocentes.Text;
+            while (listaDocentes.Count != 0)
+            {
+                docente = listaDocentes.First();
+                if (docente.Cedula == valorCedula)
+                {
+                    break;
+                }
+                else
+                {
+                    listaDocentes.RemoveFirst();
+                }
+            }//while
+              docente = docenteBusiness.obtenerDocente(docente);
+              LinkedList<Especialidad> especialidades = docente.Especialidades;
+             Especialidad especialidad= new Especialidad();
+               string salidaEspec="";
+             while (especialidades.Count != 0)
+             {
+                 especialidad = especialidades.First();
+                 salidaEspec += especialidad.Nombre;
+                 especialidades.RemoveFirst();
+                 if (especialidades.Count != 0)
+                 {
+                     salidaEspec += ", ";
+                 }
+                 else { break; }
+
+
+             }//while 
+
+
+            lbl_cedula.Text = docente.Cedula;
+            lbl_nombre.Text = docente.Nombre;
+            lbl_apellidos.Text = docente.Apellidos;
+            lbl_telefono.Text = docente.Telefono;
+            lbl_correo.Text = docente.Correo;
+            lbl_direccion.Text = docente.Direccion;
+            tb_especialidades.Text = salidaEspec;    
+        }//consultar
+    }
+}
---
+++ SistemaCAMEL/SistemaEscolar/InsertarDocenteForm.aspx.designer.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SistemaEscolar {
+    
+    
+    public partial class InsertarDocenteForm {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control lbl_cedula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_cedula;
+        
+        /// <summary>
+        /// Control lbl_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_nombre;
+        
+        /// <summary>
+        /// Control lbl_apellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_apellidos;
+        
+        /// <summary>
+        /// Control lbl_telefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_telefono;
+        
+        /// <summary>
+        /// Control lbl_correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_correo;
+        
+        /// <summary>
+        /// Control lbl_direccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_direccion;
+        
+        /// <summary>
+        /// Control tb_especialidades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tb_especialidades;
+        
+        /// <summary>
+        /// Control ddlDocentes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlDocentes;
+        
+        /// <summary>
+        /// Control btn_consultar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_consultar;
+    }
+}

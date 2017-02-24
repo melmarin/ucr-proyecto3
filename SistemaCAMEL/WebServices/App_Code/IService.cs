99e17df40923a2454dd8b8ab01950a4bdb869162
Steven Camacho <stevencorreo@yahoo.com>
2017-02-24 02:05:32 -0600

    cambios en web services... aún no se comunican como debe ser..

--- SistemaCAMEL/WebServices/App_Code/IService.cs
+++ SistemaCAMEL/WebServices/App_Code/IService.cs
@@ -10,47 +10,17 @@ using System.Text;
 [ServiceContract]
 public interface IService
 {
-
-	[OperationContract]
-	string GetData(int value);
-
-	[OperationContract]
-	CompositeType GetDataUsingDataContract(CompositeType composite);
     [OperationContract]
-    CompositeType GetDataUsingDataContracta(CompositeType composite);
+    LinkedList<string> consulta(string usuario);
 
-    // TODO: agregue aquí sus operaciones de servicio
-
-    [OperationContract]//las operaciones que realizará el servicio
-    string getTextoDePrueba();
+    [OperationContract]
+    string consulta2(string user);
 
     [OperationContract]
     string validar(Matricula parametros);
-
-
 }
 
-// Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
-[DataContract]
-public class CompositeType
-{
-	bool boolValue = true;
-	string stringValue = "Hello ";
 
-	[DataMember]
-	public bool BoolValue
-	{
-		get { return boolValue; }
-		set { boolValue = value; }
-	}
-
-	[DataMember]
-	public string StringValue
-	{
-		get { return stringValue; }
-		set { stringValue = value; }
-	}
-}
 
 [DataContract]//permite hacer la comunicación serializable y así comunicarse por la web
 public class Matricula {
--- SistemaCAMEL/WebServices/App_Code/Service.cs
+++ SistemaCAMEL/WebServices/App_Code/Service.cs
@@ -11,41 +11,19 @@ using System.Web.Configuration;
 
 public class Service : IService
 {
-
-	public string GetData(int value)
-	{
-		return string.Format("You entered: {0}", value);
-	}
-    
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
-
-    public CompositeType GetDataUsingDataContracta(CompositeType composite)
-    {
-        throw new NotImplementedException();
+    public LinkedList<string> consulta(string usuario)
+      {
+        LinkedList<string> login = new LinkedList<string>();
+        login.AddLast(usuario+"User");
+        login.AddLast(usuario +"pass4");
+        return login ;
     }
 
-    public string getTextoDePrueba()//interfaz generada
+    public string consulta2(string user)
     {
-        DocenteBusiness docente;
-        String conectionString;
-        conectionString = WebConfigurationManager.ConnectionStrings["2017_sistema_camel"].ConnectionString;
-        docente = new DocenteBusiness(conectionString);
-        LinkedList<Docente> docentes;
-       // using (docentes = docente.obtenerDocentes())
-            return "esto es una respuesta";
+        
+        return "esto es un texto del servidor-******-usuario "+user ;
     }
-
 
     public string validar(Matricula parametros)
     {
